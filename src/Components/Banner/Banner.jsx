9adc111cf6bebd421d7fbada15aa5be38fbb3d9b
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-04 19:44:41 +0600

    fix: banner responsive

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -2,10 +2,10 @@ import profileImg from "../../assets/images/Sanjid.jpg";
 
 const Banner = () => {
   return (
-    <div className="hero min-h-screen text-neutral-content my-12">
-      <div className="flex justify-center items-center px-3 lg:px-24 gap-6">
+    <div className="hero min-h-screen text-neutral-content mt-12">
+      <div className="flex justify-center items-center px-3 lg:px-24 gap-10 lg:gap-6 flex-col flex-col-reverse lg:flex-row">
         <div className="flex-1">
-          <h1 className="text-5xl font-bold">
+          <h1 className="text-4xl lg:text-5xl font-bold">
             Hi, I'm{" "}
             <span className="bg-gradient-to-r from-[#7bdaeb] to-[#00BFFF] text-transparent bg-clip-text">
               Rubaeid Sanjid
@@ -16,7 +16,7 @@ const Banner = () => {
             applications using Tailwind CSS, JavaScript, React and Node JS. I'm
             always eager to learn and take on new challenges.
           </p>
-          <button className="btn bg-gradient-to-r from-[#3672b6] to-[#00BFFF] border-none text-white text-lg uppercase">
+          <button className="btn bg-gradient-to-r from-[#3672b6] to-[#00BFFF] border-none text-white lg:text-lg uppercase">
             Contact Me
           </button>
         </div>
