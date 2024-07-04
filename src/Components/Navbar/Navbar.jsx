27840fab9313a748860f310f7837174c7a2250a3
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-04 19:37:52 +0600

    fix: navbar responsive

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -6,14 +6,13 @@ const Navbar = () => {
     <>
       <NavLink className={'text-xl mx-2'} to={'/about'}>About</NavLink>
       <NavLink className={'text-xl mx-2'} to={'/all-projects'}>Projects</NavLink>
-      <NavLink className={'text-xl mx-2'} to={'/blogs'}>Blog</NavLink>
     </>
   );
   return (
     <div className="navbar text-neutral-content px-3 lg:px-24 my-6">
       <div className="navbar-start">
         <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+          <div tabIndex={0} role="button" className="btn px-0 lg:px-1 btn-ghost lg:hidden">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               className="h-5 w-5"
@@ -31,18 +30,18 @@ const Navbar = () => {
           </div>
           <ul
             tabIndex={0}
-            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow"
+            className="menu menu-sm dropdown-content bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))] rounded-box z-[1] mt-3 w-52 p-2 shadow"
           >
             {navlink}
           </ul>
         </div>
-        <a className="btn btn-ghost text-3xl">Portfolio</a>
+        <a className="btn pl-1 btn-ghost text-2xl lg:text-3xl">Portfolio</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">{navlink}</ul>
       </div>
       <div className="navbar-end">
-        <a className="btn bg-gradient-to-r from-[#3672b6] to-[#00BFFF] border-none text-white uppercase" href={resume} download>Download Resume</a>
+        <a className="btn bg-gradient-to-r from-[#3672b6] to-[#00BFFF] border-none text-white uppercase btn-sm lg:btn-md" href={resume} download>Download Resume</a>
       </div>
     </div>
   );
