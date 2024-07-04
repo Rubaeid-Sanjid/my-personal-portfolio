d5a5667ff38663dd93d5f415d8494df624c213a0
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-05 00:33:21 +0600

    add: animation in about section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,28 +1,40 @@
+import { motion } from "framer-motion";
+
 const About = () => {
   return (
-    <div id="about" className="container mx-auto px-3 lg:px-12 text-neutral-content">
-        <div className="divider bg-neutral-content h-[1px] lg:w-1/2 w-3/4 mx-auto my-12"></div>
+    <motion.div
+      initial={{ opacity: 0, y: 100 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      transition={{ duration: 2 }}
+      id="about"
+      className="container mx-auto px-3 lg:px-12 text-neutral-content"
+    >
+      <div className="divider bg-neutral-content h-[1px] lg:w-1/2 w-3/4 mx-auto my-12"></div>
       <h2 className="text-4xl font-semibold mb-10 text-center">About Me</h2>
 
       <p className="text-lg mb-4">
-        Hello, I'm <span className="font-bold">Rubaeid Sanjid</span>, a passionate junior
-        web developer with over 6 months of experience in building dynamic and
-        responsive websites.
+        Hello, I'm <span className="font-bold">Rubaeid Sanjid</span>, a
+        passionate junior web developer with over 6 months of experience in
+        building dynamic and responsive websites.
       </p>
 
       <p className="text-lg mb-4">
-        Currently, I'm studying Computer Science and Engineering at{' '}
-        <a href="https://www.aiub.edu/" className="font-bold">American International University-Bangladesh (AIUB).</a>
+        Currently, I'm studying Computer Science and Engineering at{" "}
+        <a href="https://www.aiub.edu/" className="font-bold">
+          American International University-Bangladesh (AIUB).
+        </a>
       </p>
 
       <p className="text-lg mb-4">
-        In addition to my studies, I am constantly
-        exploring new technologies. I'm particularly
-        interested in MERN stack development and Full-stack development.
+        In addition to my studies, I am constantly exploring new technologies.
+        I'm particularly interested in MERN stack development and Full-stack
+        development.
       </p>
 
       <p className="text-lg mb-4">
-        Outside of my personal work, I enjoy traveling, gossiping with friends and family, and playing games. These hobbies help me stay creative and energized.
+        Outside of my personal work, I enjoy traveling, gossiping with friends
+        and family, and playing games. These hobbies help me stay creative and
+        energized.
       </p>
 
       <p className="text-lg mb-4">
@@ -45,7 +57,7 @@ const About = () => {
         </a>
         !
       </p>
-    </div>
+    </motion.div>
   );
 };
 
