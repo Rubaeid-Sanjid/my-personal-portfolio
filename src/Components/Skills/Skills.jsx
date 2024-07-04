f79ed35af73d91961b17d069512f470ae4464e6b
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-04 19:50:30 +0600

    fix: skill section responsive

--- src/Components/Skills/Skills.jsx
+++ src/Components/Skills/Skills.jsx
@@ -4,38 +4,36 @@ import { SiExpress, SiMongodb } from "react-icons/si";
 
 const Skills = () => {
   return (
-    <div className="mt-24">
-      <div className="divider bg-neutral-content h-[1px] w-1/2 mx-auto my-12"></div>
+    <div className="mt-8 lg:mt-24">
+      <div className="divider bg-neutral-content h-[1px] lg:w-1/2 w-3/4 mx-auto my-12"></div>
       <h2 className="text-neutral-content text-center text-4xl mb-5">Skills</h2>
       <div className="flex flex-wrap gap-6 justify-center items-center mt-10">
         <div className="p-4 border-2 rounded-2xl">
-          <FaHtml5 className="text-[#dd4b25] text-5xl" />
-          {/* <h3 className="text-neutral-content">HTML5</h3> */}
+          <FaHtml5 className="text-[#dd4b25] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <FaCss3Alt className="text-[#214ce5] text-5xl" />
-          {/* <h3 className="text-neutral-content">CSS3</h3> */}
+          <FaCss3Alt className="text-[#214ce5] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <RiTailwindCssFill className="text-[#38bdf8] text-5xl" />
+          <RiTailwindCssFill className="text-[#38bdf8] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <RiJavascriptFill className="text-[#efd81d] text-5xl" />
+          <RiJavascriptFill className="text-[#efd81d] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <FaReact className="text-[#58c4dc] text-5xl" />
+          <FaReact className="text-[#58c4dc] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <RiFirebaseFill className="text-[#f7c52b] text-5xl" />
+          <RiFirebaseFill className="text-[#f7c52b] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <FaNode className="text-[#589352] text-5xl" />
+          <FaNode className="text-[#589352] text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <SiExpress className="text-neutral-content text-5xl" />
+          <SiExpress className="text-neutral-content text-3xl lg:text-5xl" />
         </div>
         <div className="p-4 border-2 rounded-2xl">
-          <SiMongodb className="text-[#00e661] text-5xl" />
+          <SiMongodb className="text-[#00e661] text-3xl lg:text-5xl" />
         </div>
       </div>
     </div>
