064023fae38a1af1d8ccfc7e94cce02e5d797284
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2024-07-04 19:56:21 +0600

    fix: project section responsive

--- src/Components/Projects/Projects.jsx
+++ src/Components/Projects/Projects.jsx
@@ -14,14 +14,16 @@ const Projects = () => {
 
   console.log(projects.length);
   return (
-    <div>
-        <div className="divider bg-neutral-content h-[1px] w-1/2 mx-auto my-24"></div>
-        <h2 className="text-neutral-content text-center text-4xl mb-5">Projects</h2>
-        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 container mx-auto px-3 lg:px-12 my-12">
-      {projects.map((project) => (
-        <ProjectCard key={project.id} project={project}></ProjectCard>
-      ))}
-    </div>
+    <div className="mt-8 lg:my-24">
+      <div className="divider bg-neutral-content h-[1px] lg:w-1/2 w-3/4 mx-auto my-12"></div>
+      <h2 className="text-neutral-content text-center text-4xl mb-5">
+        Projects
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 container mx-auto px-3 lg:px-12 my-12">
+        {projects.map((project) => (
+          <ProjectCard key={project.id} project={project}></ProjectCard>
+        ))}
+      </div>
     </div>
   );
 };
