7caadf2f7eeaabd7aab875b3c228c7d4e5a8821d
Rubaeid-Sanjid <rubaeid.sanjid@gmail.com>
2025-01-30 21:59:29 +0600

    added marquee in certificate section

--- src/Components/Certificates/Certificates.jsx
+++ src/Components/Certificates/Certificates.jsx
@@ -1,6 +1,24 @@
+import Marquee from "react-fast-marquee";
 
 const Certificates = () => {
   const achievements = [
+    {
+      title: "Complete Web Development Course",
+      image:
+        "https://i.postimg.cc/D0JJrhKb/Certificate-of-Complete-Web-Development-Course-With-Jhankar-Mahbub.jpg",
+      link: "#",
+    },
+    {
+      title: "MySQL Tutorial",
+      image: "https://i.postimg.cc/dVLS6XXD/My-SQL-Tutorial.jpg",
+      link: "https://www.mygreatlearning.com/certificate/YOSSLYNA",
+    },
+    {
+      title: "Presentation Public Speaking",
+      image:
+        "https://i.postimg.cc/R0Dy9GzN/Certificate-of-Presentation-Public-Speaking.jpg",
+      link: "https://10minuteschool.com/certificate/671e4995149b7/",
+    },
     {
       title: "Higher Secondary Certificate (HSC)",
       image: "https://i.postimg.cc/mDgNrGBw/HSC-Certificate.jpg",
@@ -16,19 +34,6 @@ const Certificates = () => {
       image: "https://i.postimg.cc/4yjbzmCt/Deans-Award.jpg",
       link: "#",
     },
-    {
-      title: "Complete Web Development Course",
-      image:
-        "https://i.postimg.cc/D0JJrhKb/Certificate-of-Complete-Web-Development-Course-With-Jhankar-Mahbub.jpg",
-      link: "#",
-    },
-    
-    {
-      title: "Presentation Public Speaking",
-      image:
-        "https://i.postimg.cc/R0Dy9GzN/Certificate-of-Presentation-Public-Speaking.jpg",
-      link: "#",
-    },
   ];
 
   return (
@@ -39,28 +44,31 @@ const Certificates = () => {
           Certifications and Achievements
         </h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {achievements.map((achievement, index) => (
-            <a
-              key={index}
-              href={achievement.link}
-              //   target="_blank"
-              rel="noopener noreferrer"
-              className="group block bg-gray-800 rounded-lg shadow-lg p-4 transform transition hover:scale-105"
-            >
-              <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg">
-                <img
-                  src={achievement.image}
-                  alt={achievement.title}
-                  className="w-full h-full object-cover group-hover:opacity-90"
-                />
-              </div>
-              <h3 className="text-center text-lg font-semibold mt-3 group-hover:text-blue-400">
-                {achievement.title}
-              </h3>
-            </a>
-          ))}
-        </div>
+        <Marquee pauseOnHover>
+          <div className="flex gap-6 overflow-hidden">
+            <div></div>
+            {achievements.map((achievement, index) => (
+              <a
+                key={index}
+                href={achievement.link}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="group block bg-gray-800 rounded-lg shadow-lg p-4 transform transition hover:scale-105 w-64"
+              >
+                <div className="aspect-w-1 aspect-h-1 w-full overflow-hidden rounded-lg">
+                  <img
+                    src={achievement.image}
+                    alt={achievement.title}
+                    className="w-full h-full object-cover group-hover:opacity-90"
+                  />
+                </div>
+                <h3 className="text-center text-lg font-semibold mt-3 group-hover:text-blue-400">
+                  {achievement.title}
+                </h3>
+              </a>
+            ))}
+          </div>
+        </Marquee>
       </div>
     </section>
   );
